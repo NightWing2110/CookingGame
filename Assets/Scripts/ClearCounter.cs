f9a77442700fc1a8276f926e039aa57cbfbe045e
Hoàng Duy <luuquanghoangdy@gmail.com>
2023-02-21 11:03:55 +0700

    Player PickUp Drop Object

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -9,6 +9,31 @@ public class ClearCounter : BaseCounter
 
     public override void Interact(Player player)
     {
-        
+        if (!HasKitchenObject())
+        {
+            //There is no KitchenObject here
+            if (player.HasKitchenObject())
+            {
+                //Player is carrying something
+                player.GetKitchenObject().SetKitchenObjectParent(this);
+            }
+            else
+            {
+                //PLayer is not carrying anything
+            }
+        }
+        else
+        {
+            // There is a KitchenObject here
+            if (player.HasKitchenObject())
+            {
+                //Player is carrying something
+            }
+            else
+            {
+                //Player is not carrying anything
+                GetKitchenObject().SetKitchenObjectParent(player);
+            }
+        }
     }
 }
